a8ac44c69179078bee6a27b465b4b70ab05251d3
R Thompson <thompson9395681@gmail.com>
2026-01-27 16:31:03 -0500

    Fix: Refresh subscription status when app resumes from Stripe Checkout

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart' as provider;
 import 'package:artiq_flutter/src/services/auth_service.dart';
 import 'package:artiq_flutter/src/screens/design_gallery_screen.dart';
+import 'package:artiq_flutter/src/providers/subscription_provider.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // Refresh subscription when app resumes (user returns from Stripe Checkout)
+    if (state == AppLifecycleState.resumed) {
+      provider.Provider.of<SubscriptionProvider>(context, listen: false)
+          .loadSubscription();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final authService = ref.watch(authServiceProvider);
 
     return Scaffold(
