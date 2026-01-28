2e26c5783bc3bdf82b8d8acbd7c27b25a9fe756c
Roy939 <Roy939@users.noreply.github.com>
2026-01-28 13:11:05 +0000

    deploy: d1b2b21e4b12bb6169b3041c6ff75a48b4b2600a

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase configuration for web
+// This file is loaded by index.html and provides Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9ySm9U55qaKta-UOvvK3mDr8yNfJudt0",
+  authDomain: "artiq-1ebb2.firebaseapp.com",
+  projectId: "artiq-1ebb2",
+  storageBucket: "artiq-1ebb2.firebasestorage.app",
+  messagingSenderId: "280578944842",
+  appId: "1:280578944842:web:717a6184419c889ea7dbb3",
+  measurementId: "G-MFZ2Z21SSV"
+};
+
+// Make config available globally
+window.firebaseConfig = firebaseConfig;
---
+++ icons/README.md
@@ -0,0 +1,12 @@
+# Web Icons
+
+This directory should contain the following icon files for the web app:
+
+- `Icon-192.png` - 192x192 app icon
+- `Icon-512.png` - 512x512 app icon
+- `Icon-maskable-192.png` - 192x192 maskable icon
+- `Icon-maskable-512.png` - 512x512 maskable icon
+
+These icons will be generated automatically when you run `flutter build web`.
+
+For now, Flutter will use default icons if these are not present.
